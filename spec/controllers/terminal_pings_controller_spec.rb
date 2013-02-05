582643f9031890d8fb250fe1c211697b08bb3a49
Sergey Gridassov <grindars@gmail.com>
2013-02-05 15:56:02 +0400

    Added specifications for providers synchronization.

--- spec/controllers/terminal_pings_controller_spec.rb
+++ spec/controllers/terminal_pings_controller_spec.rb
@@ -19,13 +19,72 @@ describe TerminalPingsController do
       :cash_acceptor => {
         'error' => '-1'
       },
-      :providers => {
-        :updated_at => DateTime.now,
-        :ids => [1, 2, 3]
-      }
+      :providers_updated_at => DateTime.now,
     }
-    @terminal.pings.count.should == 1
+    @terminal.pings.count.should >= 1
     @terminal.pings.first.cash_sum.should == 1140
     @terminal.pings.first.ip.should == '127.0.0.1'
+  end
+
+  it "updates outdated providers" do
+    post :create, :terminal => 'test', :terminal_ping => {
+      :providers_updated_at => nil
+    }
+
+    body = ActiveSupport::JSON.decode response.body
+    body["update_providers"].should == true
+  end
+
+  it "properly handles actual providers" do
+    post :create, :terminal => 'test', :terminal_ping => {
+      :providers_updated_at => DateTime.now,
+    }
+
+    body = ActiveSupport::JSON.decode response.body
+    body["update_providers"].should == false
+  end
+
+  it "delivers correct data" do
+    get :providers, :terminal => 'test'
+
+    body = ActiveSupport::JSON.decode ActiveSupport::Gzip.decompress(response.body)
+    DateTime.parse(body["updated_at"]).to_i.should == @terminal.terminal_profile.actual_timestamp.to_i
+  end
+
+  it "properly handles update scenario" do
+    initial_stamp = @terminal.terminal_profile.actual_timestamp
+
+    post :create, :terminal => 'test', :terminal_ping => {
+      :providers_updated_at => initial_stamp
+      }
+
+    body = ActiveSupport::JSON.decode response.body
+    body["update_providers"].should == false
+
+    sleep 1
+
+    @terminal.terminal_profile.invalidate_cached_providers!
+
+    post :create, :terminal => 'test', :terminal_ping => {
+      :providers_updated_at => initial_stamp
+    }
+
+    body = ActiveSupport::JSON.decode response.body
+    body["update_providers"].should == true
+
+    get :providers, :terminal => 'test'
+
+    body = ActiveSupport::JSON.decode ActiveSupport::Gzip.decompress(response.body)
+    new_stamp = DateTime.parse(body["updated_at"])
+    new_stamp.should > initial_stamp
+    new_stamp.to_i.should == @terminal.terminal_profile.actual_timestamp.to_i
+
+    post :create, :terminal => 'test', :terminal_ping => {
+      :providers_updated_at => new_stamp
+    }
+
+    body = ActiveSupport::JSON.decode response.body
+    body["update_providers"].should == false
+
   end
 end
